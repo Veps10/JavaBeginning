d77c769b5dd2a72c6420b1d9562c17531c93a233
Veps10 <149360339+Veps10@users.noreply.github.com>
2023-11-18 11:27:43 +0200

    Create DayofWeek.java

---
+++ DayofWeek.java
@@ -0,0 +1,23 @@
+public class DayofWeek {
+    public static void main(String[] args) {
+        int DayofWeekNum = 8;
+        switch (DayofWeekNum) {
+            case 1:
+                System.out.println("MON");
+                break;
+            case 2:
+                System.out.println("TUE");
+                break;
+            case 3:
+                System.out.println("WEN");
+                break;
+            case 4:
+                System.out.println("THR");
+
+            default:
+                System.out.println("You are wrong.");
+                break;
+        }
+    }
+
+}
