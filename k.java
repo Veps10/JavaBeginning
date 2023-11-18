6442c09cc7f547371be721da547dfce7d039d741
Veps10 <149360339+Veps10@users.noreply.github.com>
2023-11-18 11:28:01 +0200

    Create k.java

---
+++ k.java
@@ -0,0 +1,7 @@
+public class k {
+    public static void main(String[] args) {
+        int i = -10 * -1 + (-2);
+        String answer = i < 0 ? "true" : "False";
+        System.out.println(answer);
+    }
+}
