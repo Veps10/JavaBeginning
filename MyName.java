a7144732be448680af1e21ac7ad002feb98a265f
Veps10 <149360339+Veps10@users.noreply.github.com>
2023-11-18 17:59:31 +0200

    Create MyName.java

---
+++ MyName.java
@@ -0,0 +1,8 @@
+public class MyName {
+    public static void main(String[] args) {
+        var firstAndLastName = "Demid Inin";
+        var nameLength = firstAndLastName.length() - 1;
+        var char0 = firstAndLastName.charAt(nameLength);
+        System.out.println(char0);
+    }
+}
