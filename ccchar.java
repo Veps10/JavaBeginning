1a022b9ff6fb61c560810fae531a24cf4d9fbd75
Veps10 <149360339+Veps10@users.noreply.github.com>
2023-11-18 11:27:39 +0200

    Create ccchar.java

---
+++ ccchar.java
@@ -0,0 +1,9 @@
+public class ccchar {
+    public static void main(String[] args) {
+
+        var one = "Monday";
+        var char1 = one.charAt(1);
+        var char2 = one.charAt(3);
+        System.out.println(char1 + char2);
+    }
+}
