da060f5185453555aae08e781486f15949dc4472
Veps10 <149360339+Veps10@users.noreply.github.com>
2023-11-19 23:05:07 +0200

    Create dates.java

---
+++ dates.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class dates {
+    public static void main(String[] args) {
+        LocalDate dateFrom = LocalDate.of(2020, 8, 12);
+        LocalDate dateTo = LocalDate.of(2023, 11, 19);
+        long noOfDaysBetween = ChronoUnit.DAYS.between(dateFrom, dateTo);
+        System.out.println(noOfDaysBetween);
+    }
+}
