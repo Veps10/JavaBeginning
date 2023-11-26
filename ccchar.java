822de62327d60273cbdf09fe2e31d657b8f87e58
Veps10 <149360339+Veps10@users.noreply.github.com>
2023-11-26 22:13:31 +0200

    Update ccchar.java

--- ccchar.java
+++ ccchar.java
@@ -2,8 +2,7 @@ public class ccchar {
     public static void main(String[] args) {
 
         var one = "Monday";
-        var char1 = one.charAt(1);
-        var char2 = one.charAt(3);
-        System.out.println(char1 + char2);
+        var firstday = one.substring(0, 3);
+        System.out.println(firstday);
     }
 }
